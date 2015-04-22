dfe6fb4a8e570a3ce65787048dada98a9fe2a9ae
Lars-Oliver Willrich <lars-oliver.willrich@fokus.fraunhofer.de>
2015-04-22 13:08:24 +0200

    fix problem with w.height

--- app/modules/map/directives/directives.js
+++ app/modules/map/directives/directives.js
@@ -6,15 +6,16 @@ angular.module('app.dashboard.map.directives', ['app.socket', 'app.config'])
 
     .directive('resize', ['$window', function ($window) {
     return function (scope, element) {
-        var w = angular.element($window);
+        var w = angular.element(window);
         scope.getWindowDimensions = function () {
-            return { 'h': $window.screen.height, 'w': $window.screen.width };
+            return { 'h': w.height(), 'w': w.width() };
         };
         scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
             scope.windowHeight = newValue.h;
             scope.windowWidth = newValue.w;
 
             scope.addStyleToMap = function () {
+                console.log("height: " + (newValue.h - 150) + 'px' + " width: " + (newValue.w) + 'px');
                 return {
                     'height': (newValue.h - 150) + 'px',
                     'width': (newValue.w) + 'px'
