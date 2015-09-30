ab7b6c9d04e9d272ee2196bb941dde4f86a534b6
Manuel Polzhofer <manuel.polzhofer@fokus.fraunhofer.de>
2015-09-30 17:13:13 +0200

    show frontend dummy chart

--- app/modules/common/services/Socket.js
+++ app/modules/common/services/Socket.js
@@ -67,7 +67,7 @@ angular.module('app.socket', ['ngWebsocket', 'app.config', 'app.dashboard.map.se
 
                //sample call
 
-                    /*         var sample4Charts = {"type":"chartObject","elements":[
+                            var sample4Charts = {"type":"chartObject","elements":[
                     {"chart":{"title":"Parking Occupancy Rate","options":{"chart":{"type":"discreteBarChart","height":240,"width":380,"margin":{"top":20,"right":20,"bottom":60,"left":55},"showValues":true,"xAxis":{"axisLabel":"Parking Slots"},"yAxis":{"axisLabel":"Amount of Cars","axisLabelDistance":30}}},"data":[{"key":"Cumulative Return","values":[{"label":"Free","value":1532,"color":"#A5C989"},{"label":"Not Free","value":4321,"color":"#D16D82"}]}]}}
                     ,{"chart":{"title":"Parking Occupancy Rate","options":{"chart":{"type":"discreteBarChart","height":240,"width":380,"margin":{"top":20,"right":20,"bottom":60,"left":55},"showValues":true,"xAxis":{"axisLabel":"Parking Slots"},"yAxis":{"axisLabel":"Amount of Cars","axisLabelDistance":30}}},"data":[{"key":"Cumulative Return","values":[{"label":"Free","value":1532,"color":"#A5C989"},{"label":"Not Free","value":4321,"color":"#D16D82"}]}]}}
                     ,{"chart":{"title":"Parking Situation","options":{"chart":{"type":"pieChart","height":320,"showLabels":true,"transitionDuration":500,"labelThreshold":0.01,"legend":{"margin":{"top":5,"right":35,"bottom":5,"left":0}}}},"data":[{"label":"Free Parking Slots","value":500,"color":"#5D8896"},{"label":"Fee Parking Slots","value":3200,"color":"#5AC3E6"},{"label":"Clock Parking Slots","value":1300,"color":"#9CCBDB"}]}},{"chart":{"title":"Average Occupancy","options":{"chart":{"type":"discreteBarChart","height":240,"width":380,"margin":{"top":20,"right":20,"bottom":60,"left":55},"showValues":true,"xAxis":{"axisLabel":"Parking Slot Type"},"yAxis":{"axisLabel":"Percentage","axisLabelDistance":30}}},"data":[{"key":"Cumulative Return","values":[{"label":"Fee Slots","value":80,"color":"#D1D665"},{"label":"Clock Slots","value":60,"color":"#959951"}]}]}}]};
@@ -79,7 +79,7 @@ angular.module('app.socket', ['ngWebsocket', 'app.config', 'app.dashboard.map.se
                     ,{"chart":{"title":"Parking Situation","options":{"chart":{"type":"pieChart","height":320,"showLabels":true,"transitionDuration":500,"labelThreshold":0.01,"legend":{"margin":{"top":5,"right":35,"bottom":5,"left":0}}}},"data":[{"label":"Free Parking Slots","value":500,"color":"#5D8896"},{"label":"Fee Parking Slots","value":3200,"color":"#5AC3E6"},{"label":"Clock Parking Slots","value":1300,"color":"#9CCBDB"}]}},{"chart":{"title":"Average Occupancy","options":{"chart":{"type":"discreteBarChart","height":240,"width":380,"margin":{"top":20,"right":20,"bottom":60,"left":55},"showValues":true,"xAxis":{"axisLabel":"Parking Slot Type"},"yAxis":{"axisLabel":"Percentage","axisLabelDistance":30}}},"data":[{"key":"Cumulative Return","values":[{"label":"Fee Slots","value":80,"color":"#D1D665"},{"label":"Clock Slots","value":60,"color":"#959951"}]}]}}]};
 
 
-                mapService.updateCharts(sample3Chart); */
+                mapService.updateCharts(sample3Chart); 
 
             }
 
